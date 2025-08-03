3ac0e147e539a4a794a06e6b04a67b0d280d8560
Danny-Dalo <ighodalodanny7@gmail.com>
2025-08-02 22:53:58 -0700

    config file

--- api_training2/config.py
+++ api_training2/config.py
@@ -1,5 +1,14 @@
+
+
+
+
+
 from dotenv import load_dotenv
 import os
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 from pathlib import Path
 
 # Get the root directory of the project
@@ -8,12 +17,14 @@ ROOT_DIR = Path(__file__).parent.parent
 # Load environment variables from .env file in the root directory
 load_dotenv(dotenv_path=ROOT_DIR / '.env')
 
-# Get the API key
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_API_KEY = "AIzaSyB3Ft4l3D1qyQDXpcLjEE5QdKHNXF4o3Zc"
-print(f"API KEY LOADED")
+# Get the API key from environment variables
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
+print("=======================LOADED==========================")
 
 if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY is not set in the .env file. Please add your API key to the .env file.")
-
+    logger.error("Error with API key")
+    raise ValueError(
+        "GEMINI_API_KEY is not set in the .env file. "
+        "Please add your API key to the .env file."
+    )
