67576725360b45a6ed555b8ed9baace63595c84a
Danny-Dalo <ighodalodanny7@gmail.com>
2026-01-21 22:51:30 +0100

    Added new middleware to validate file size before it is taken in and processed by the upload route.

--- app_quiksight/main.py
+++ app_quiksight/main.py
@@ -1,15 +1,39 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, status
 from .routes import upload, chat
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 import logging
-
-logger = logging.getLogger(__name__)
 
 templates = Jinja2Templates("app_quiksight/templates")
 
+
+from starlette.middleware.base import BaseHTTPMiddleware
+MAX_SIZE = 30 * 1024 * 1024  # 30 MB
+# Checks file size before it hits upload endpoint logic
+class LimitUploadSizeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if request.method == "POST":
+            content_length = request.headers.get("content-length")
+            if content_length:
+                if int(content_length) > MAX_SIZE:
+                    return templates.TemplateResponse(
+                        "home.html", 
+                        {
+                            "request": request, 
+                            "error": "File too large. Maximum size limit is 30MB."
+                        },
+                        status_code=413
+                    )
+        
+        return await call_next(request)
+
+
+logger = logging.getLogger(__name__)
+
+
 app = FastAPI()
+app.add_middleware(LimitUploadSizeMiddleware)
 
 
 app.mount("/static", StaticFiles(directory="app_quiksight/static"), name="static")
